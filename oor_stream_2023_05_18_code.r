779cb2a5da7a32dd352e69ef9b9867ff41fcb802
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-05-18 22:22:05 +0200

    Commit: Thu 18 May 2023 10:22:05 PM CEST

--- oor_stream_2023_05_18_code.r
+++ oor_stream_2023_05_18_code.r
@@ -36,6 +36,9 @@ plot(dat$Year, dat$Rates, type="n", bty="l",
 grid()
 lines(dat$Year, dat$Rates, lwd=3)
 
+# make a copy of dat for Figure 2.11c
+sav <- dat
+
 # read in the mortality data again
 dat <- read.table("white_nonhisp_death_rates_from_1999_to_2013.txt", header=TRUE)
 
@@ -61,87 +64,10 @@ rates.adj <- sapply(years, function(year) {
    weighted.mean(rates2013, dat$Population[dat$Year == year])
 })
 
-rates.adj
-
-
-
-
-
-
-
-
-
-
-# read in the birth data
-dat <- read.table("births.txt", header=TRUE)
-
-# compute the mean age within the 45 to 54 age group in the years 1999 to 2013
-years <- 1999:2013
-mage <- sapply(years, function(year) {
-
-   # the age group we are interested in
-   ages <- 54:45
-
-   # check which birth years correspond to these ages
-   ok <- dat$year %in% (year - ages)
-
-   # compute the weight mean of the ages with weights equal to the number of
-   # births corresponding to these ages (note: we need to specify the ages
-   # above from oldest to youngest, so that the birth numbers correspond to
-   # the ages)
-   weighted.mean(ages, dat$births[ok])
-
-})
-
-# print the mean ages
-mage
-
-# Figure 2.11b
-plot(years, mage, type="n", bty="l",
-     xlab="", ylab="Avg age among non-Hisp whites 45-54")
-grid()
-lines(years, mage, lwd=3)
-
-# note: this seems to assume that everybody born in a particular year survives
-# until they are 45-54 years old, which seems like a debatable assumption
-
-# mortality rates in 2000 by age (overall and separately by gender) from life
-# tables as given in the code for this example on the book website
-deathpr_by_age <- c(.003064, .003322, .003589, .003863, .004148, .004458, .004800, .005165, .005554, .005971)
-deathpr_male   <- c(.003244, .003571, .003926, .004309, .004719, .005156, .005622, .006121, .006656, .007222)
-deathpr_female <- c(.002069, .002270, .002486, .002716, .002960, .003226, .003505, .003779, .004040, .004301)
-
-# read in the data
-dat <- read.csv("US-EST00INT-ALLDATA.csv")
-
-rate.adjusted <- sapply(years, function(year) {
-
-   # calculate how old people are in the year 2000 for people who were 45 to
-   # 54 years old in a particular year
-   ages_in_2000 <- (2000-year) + (45:54)
-
-   # check which rows correspond to these ages in the year 2000 (and only take
-   # the April data)
-   ok <- dat$AGE %in% ages_in_2000 & dat$YEAR==2000 & dat$MONTH==4
-
-   #dat[ok,c("AGE","NHWA_MALE","NHWA_FEMALE")]
-
-   # compute the weighted mean of the mortality rates in 2000 weighting by the
-   # number of non-Hispanic whites that were between 45 and 54 in that year
-   #weighted.mean(deathpr_by_age, dat$NHWA_MALE[ok] + dat$NHWA_FEMALE[ok])
-   sum(deathpr_male * dat$NHWA_MALE[ok] + deathpr_female * dat$NHWA_FEMALE[ok]) / sum(dat$NHWA_MALE[ok] + dat$NHWA_FEMALE[ok])
-
-})
-
 # Figure 2.11c
 plot(sav$Year, sav$Rates, type="n", bty="l",
      xlab="", ylab="Death rate among non-Hisp whites 45-54")
 grid()
 lines(sav$Year, sav$Rates, lwd=3)
-lines(years, rate.adjusted, lwd=3)
+lines(years, rates.adj, lwd=3)
 
-
-plot(years, rate.adjusted, type="n", bty="l",
-     xlab="", ylab="Death rate among non-Hisp whites 45-54")
-grid()
-lines(years, rate.adjusted, lwd=3)
