6b401cd443dbf6070d23f43c43b09f7a069d0502
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-05-18 20:45:48 +0200

    Commit: Thu 18 May 2023 08:45:48 PM CEST

--- oor_stream_2023_05_18_code.r
+++ oor_stream_2023_05_18_code.r
@@ -21,13 +21,24 @@ download.file("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Ag
 # read in the data
 dat <- read.table("births.txt", header=TRUE)
 
-mage <- sapply(1989:2015, function(year) {
+# compute the mean age within the 45 to 54 age group in the years 1989 to 2015
+years <- 1999:2013
+mage <- sapply(years, function(year) {
    # the age group we are interested in
    ages <- 54:45
    # check which birth years correspond to these ages
    ok <- dat$year %in% (year - ages)
    # compute the weight mean of the ages with weights equal to the number of
-   # births corresponding to these ages
+   # births corresponding to these ages (note: we need to specify the ages
+   # above from oldest to youngest, so that the birth numbers correspond to
+   # the ages)
    weighted.mean(ages, dat$births[ok])
 })
+
 mage
+
+# Figure 2.11(c)
+plot(years, mage, type="n", bty="l",
+     xlab="", ylab="Avg age among non-Hisp whites 45-54")
+grid()
+lines(years, mage, lwd=3)
