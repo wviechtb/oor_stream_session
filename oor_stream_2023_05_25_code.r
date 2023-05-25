ec817f0eb4cf382ad6a45f24636669a93f703297
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-05-25 18:21:52 +0200

    Commit: Thu 25 May 2023 06:21:52 PM CEST

--- oor_stream_2023_05_25_code.r
+++ oor_stream_2023_05_25_code.r
@@ -14,3 +14,19 @@
 
 ############################################################################
 
+### 5.1: Arrays
+
+# create a vector with 1500 random draws from a standard normal distribution
+z <- rnorm(1500)
+z
+
+# attach three dimensions to this vector with the first dimension having
+# length 3, the second length 5, and the third length 100
+dim(z) <- c(3,5,100)
+z
+
+# check what the attributes of z are
+attributes(z)
+
+# we see that z now as a dimension attribute, which is the 'dimension vector'
+
