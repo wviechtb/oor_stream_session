c8a3636698caabdadf80bb4fe432020bbc79a2af
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-06-01 17:56:10 +0200

    Commit: Thu 01 Jun 2023 05:56:10 PM CEST

---
+++ oor_stream_2023_06_01_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-06-01
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 3.1 - ?
+#
+# last updated: 2023-06-01
+
+############################################################################
+
+### 3.1: Weighted averages
+
