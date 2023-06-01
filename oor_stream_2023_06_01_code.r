c56ac54ca2bc788b68bbf976dfdc3dfbf50696f9
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-06-01 18:22:13 +0200

    Commit: Thu 01 Jun 2023 06:22:13 PM CEST

--- oor_stream_2023_06_01_code.r
+++ oor_stream_2023_06_01_code.r
@@ -16,4 +16,14 @@
 ### 3.1: Weighted averages
 
 # weighted average of the ages of people in the US, Mexico, and Canada
-(310000000 ∗ 36.8 + 112000000 ∗ 26.7 + 34000000 ∗ 40.7) / (310000000 + 112000000 + 34000000)
+(310000000 * 36.8 + 112000000 * 26.7 + 34000000 * 40.7) / (310000000 + 112000000 + 34000000)
+
+# create vectors with the mean age values and population sizes
+mean.age <- c(36.8, 26.7, 40.7)
+pop.size <- c(310000000, 112000000, 34000000)
+
+# use weighted.mean() to obtain the weighted average
+weighted.mean(mean.age, pop.size)
+
+# the weights that are used are
+pop.size / sum(pop.size)
