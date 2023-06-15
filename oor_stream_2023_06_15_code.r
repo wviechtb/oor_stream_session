3dd9e15ce9d953c5d38b05555237408b00580c6b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-06-15 19:14:09 +0200

    Commit: Thu 15 Jun 2023 07:14:09 PM CEST

--- oor_stream_2023_06_15_code.r
+++ oor_stream_2023_06_15_code.r
@@ -77,9 +77,8 @@ z
 z <- list(c(1,4,6), animal="Chicken", diag(4))
 z
 
-# sidenote: while using abbreviate component names with the $ notation seems
+# sidenote: while using abbreviated component names with the $ notation seems
 # convenient, it is also a potential source of errors or confusion; we can
-# instruct R to give us a warning whenever abbreviated component names are
-# used
+# instruct R to give us a warning whenever abbreviated component names are used
 options(warnPartialMatchDollar=TRUE)
 Lst$child
