45de9306cf0021cc83ad08e3b52ad2bbd4c5028c
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-06-15 17:42:53 +0200

    Commit: Thu 15 Jun 2023 05:42:53 PM CEST

---
+++ oor_stream_2023_06_15_code.r
@@ -0,0 +1,16 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-06-15
+#
+# Topic(s):
+# - An Introduction to R
+#   https://cran.r-project.org/doc/manuals/r-release/R-intro.html
+# - Section(s): 6.1 - ?
+#
+# last updated: 2023-06-15
+
+############################################################################
+
