6d6b10385cb9eb258ee328d5a23b4a8dc4ed385c
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-06-22 19:45:32 +0200

    Commit: Thu 22 Jun 2023 07:45:32 PM CEST

--- oor_stream_2023_06_22_code.r
+++ oor_stream_2023_06_22_code.r
@@ -23,9 +23,9 @@ abline(v=63.7, lty="dotted")
 
 # shade in the area of the distribution where height is 60 inches or below
 xs.sub <- seq(55, 60, length=1000)
-ys.sub <- dnorm(xs, mean=63.7, sd=2.7)
+ys.sub <- dnorm(xs.sub, mean=63.7, sd=2.7)
 polygon(c(xs.sub,60,rev(xs.sub)), c(ys.sub,0,rep(0,1000)), col="lightgray", border=NA)
-lines(xs.sub, ys.sub, lwd=3)
+lines(xs, ys, lwd=3)
 
 # calculate the size of the shaded area
 pnorm(60, mean=63.7, sd=2.7)
@@ -45,3 +45,18 @@ hist(height, breaks=100)
 
 # the proportion of women in the sample who are 60 inches or shorter
 mean(height <= 60)
+
+# redraw the normal distribution and shade in more areas
+xs <- seq(55, 74, length=1000)
+ys <- dnorm(xs, mean=63.7, sd=2.7)
+plot(xs, ys, type="l", xlab="height (inches)", ylab="density", lwd=3)
+xs.sub <- seq(63.7-3*2.7, 63.7+3*2.7, length=1000)
+ys.sub <- dnorm(xs.sub, mean=63.7, sd=2.7)
+polygon(c(xs.sub,63.7+3*2.7,rev(xs.sub)), c(ys.sub,0,rep(0,1000)), col="gray30", border=NA)
+xs.sub <- seq(63.7-2*2.7, 63.7+2*2.7, length=1000)
+ys.sub <- dnorm(xs.sub, mean=63.7, sd=2.7)
+polygon(c(xs.sub,63.7+2*2.7,rev(xs.sub)), c(ys.sub,0,rep(0,1000)), col="gray60", border=NA)
+xs.sub <- seq(63.7-1*2.7, 63.7+1*2.7, length=1000)
+ys.sub <- dnorm(xs.sub, mean=63.7, sd=2.7)
+polygon(c(xs.sub,63.7+1*2.7,rev(xs.sub)), c(ys.sub,0,rep(0,1000)), col="gray90", border=NA)
+lines(xs, ys, lwd=3)
