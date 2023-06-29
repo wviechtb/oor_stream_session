8cde7af72d03efd63c830dd59d7d6e6ebd65bec1
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-06-29 17:14:13 +0200

    Commit: Thu 29 Jun 2023 05:14:13 PM CEST

---
+++ oor_stream_2023_06_29_code.r
@@ -0,0 +1,18 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-06-29
+#
+# Topic(s):
+# - An Introduction to R
+#   https://cran.r-project.org/doc/manuals/r-release/R-intro.html
+# - Section(s): 7.1 - ?
+#
+# last updated: 2023-06-29
+
+############################################################################
+
+### 7.1: The read.table() function
+
