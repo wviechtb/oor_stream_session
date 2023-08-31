61597ba83dffd411b3534feff2b27662b30e2640
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-08-31 21:25:15 +0200

    Commit: Thu 31 Aug 2023 09:25:14 PM CEST

--- oor_stream_2023_08_31_code.r
+++ oor_stream_2023_08_31_code.r
@@ -180,4 +180,7 @@ hist(dat$eruptions, col="gray30")
 hist(dat$eruptions, breaks=30, col="gray30")
 
 # we can also specify the exact position of the break points
-hist(dat$eruptions, breaks=seq(0,6,by=0.125), col="gray30")
+hist(dat$eruptions, breaks=seq(1,6,by=0.125), col="gray30")
+
+hist(dat$eruptions, breaks=seq(1,6,by=0.125),
+     xlab="Eruption Time (in minutes)", main="", col="gray30")
