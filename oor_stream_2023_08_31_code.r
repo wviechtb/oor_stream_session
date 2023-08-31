22105795eea2a2f9bf2a0ff650dc23b4a24afa8a
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-08-31 19:28:45 +0200

    Commit: Thu 31 Aug 2023 07:28:45 PM CEST

--- oor_stream_2023_08_31_code.r
+++ oor_stream_2023_08_31_code.r
@@ -14,3 +14,22 @@
 
 ############################################################################
 
+### 8.1: R as a set of statistical tables
+
+## normal distribution
+
+# calculate the density of standard normal distribution for values between -4
+# and 4 and then plot the distribution (note: mean=0 and sd=1 are the
+# defaults, so we could leave those out)
+xs <- seq(-4, 4, length=10000)
+ys <- dnorm(xs, mean=0, sd=1)
+plot(xs, ys, type="l", lwd=2, bty="l")
+
+# what proportion of the distribution falls below -1
+pnorm(-1)
+
+# shade in the corresponding area in the plot
+xs <- seq(-4, -1, length=10000)
+ys <- dnorm(xs, mean=0, sd=1)
+polygon(c(xs,rev(xs)), c(ys,rep(0,10000)), col="lightgray")
+
