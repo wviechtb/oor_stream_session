50450d35d5fa97b0ff25d84860bd2c2fcbba48e4
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-09-07 18:38:09 +0200

    Commit: Thu 07 Sep 2023 06:38:09 PM CEST

---
+++ oor_stream_2023_09_07_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-09-07
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 4.2 - ?
+#
+# last updated: 2023-09-07
+
+############################################################################
+
+### 4.2: Estimates, standard errors, and confidence intervals
+
