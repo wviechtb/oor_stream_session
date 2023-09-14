80213aeaa8d24017746a02c512d3df98ff01003f
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-09-14 18:40:20 +0200

    Commit: Thu 14 Sep 2023 06:40:20 PM CEST

--- oor_stream_2023_09_14_code.r
+++ oor_stream_2023_09_14_code.r
@@ -21,3 +21,12 @@ A <- c(79.98,80.04,80.02,80.04,80.03,80.03,80.04,79.97,80.05,80.03,80.02,80.00,8
 B <- c(80.02,79.94,79.98,79.97,79.97,80.03,79.95,79.97)
 A
 B
+
+# side-by-side boxplots of the two variables
+boxplot(A, B)
+
+# make it look a bit nicer
+par(bty="l")
+boxplot(list(A=A, B=B), xlab="Method", ylab="Latent Heat (cal/gm)")
+
+
