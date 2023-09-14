78bd4ac1498ecb7917bd63fceddc53a5459a3a71
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-09-14 18:30:19 +0200

    Commit: Thu 14 Sep 2023 06:30:19 PM CEST

--- oor_stream_2023_09_14_code.r
+++ oor_stream_2023_09_14_code.r
@@ -16,3 +16,8 @@
 
 ### 8.3: One- and two-sample tests
 
+# create vectors A and B with the data shown (could also use scan())
+A <- c(79.98,80.04,80.02,80.04,80.03,80.03,80.04,79.97,80.05,80.03,80.02,80.00,80.02)
+B <- c(80.02,79.94,79.98,79.97,79.97,80.03,79.95,79.97)
+A
+B
