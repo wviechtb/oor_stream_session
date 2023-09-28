a28a46dcb981a5738e6f653446ce5aa12d70ba98
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-09-28 20:01:56 +0200

    Commit: Thu 28 Sep 2023 08:01:56 PM CEST

--- oor_stream_2023_09_28_code.r
+++ oor_stream_2023_09_28_code.r
@@ -80,12 +80,18 @@ curve(dchisq(x, df=n-1), add=TRUE, lwd=5)
 # from this, we can derive the distribution of the variance
 hist(stats[2,]^2, breaks=80, xlab="Variance",
      main="Sampling Distribution of the Variance", freq=FALSE)
-curve(dchisq(x * (n-1) / 10^2, df=n-1) * (n-1) / 10^2, add=TRUE, lwd=5)
+curve(dchisq(y * (n-1) / 10^2, df=n-1) * (n-1) / 10^2, add=TRUE, lwd=5, xname="y")
+
+# https://online.stat.psu.edu/stat414/lesson/23/23.1
+# x = sd^2 * (n-1) / sigma^2
+# y = x / (n-1) * sigma^2
+# x = y * (n-1) / sigma^2
+# dx/dy = (n-1) / sigma^2
 
 # and from this, we can derive the distribution of the standard deviation
 hist(stats[2,], breaks=80, xlab="Standard Deviation",
      main="Sampling Distribution of the Standard Deviation", freq=FALSE)
-curve(dchisq(x^2 * (n-1) / 10^2, df=n-1) * 2 * x * (n-1) / 10^2, add=TRUE, lwd=5)
+curve(dchisq(y^2 * (n-1) / 10^2, df=n-1) * 2 * y * (n-1) / 10^2, add=TRUE, lwd=5, xname="y")
 
 # x = sd^2 * (n-1) / sigma^2
 # y = sqrt(x / (n-1) * sigma^2)
