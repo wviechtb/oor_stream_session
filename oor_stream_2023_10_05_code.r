0faa56958fffb610a21b9d6a8eb3b7fbfde99b42
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-10-05 17:53:33 +0200

    Commit: Thu 05 Oct 2023 05:53:33 PM CEST

---
+++ oor_stream_2023_10_05_code.r
@@ -0,0 +1,16 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-10-05
+#
+# Topic(s):
+# - An Introduction to R
+#   https://cran.r-project.org/doc/manuals/r-release/R-intro.html
+# - Section(s): 11.2 - ?
+#
+# last updated: 2023-10-05
+
+############################################################################
+
