700722b0b3697e1b9a7e5f707164dab40600e27e
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-10-19 18:01:07 +0200

    Commit: Thu 19 Oct 2023 06:01:07 PM CEST

---
+++ oor_stream_2023_10_19_code.r
@@ -0,0 +1,15 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-10-19
+#
+# Topic(s):
+# - Recent Updates to the metafor Package
+# - https://www.metafor-project.org
+#
+# last updated: 2023-10-19
+
+############################################################################
+
