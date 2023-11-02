a1aac81142e1318dab3583a778314f90f9babe17
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-11-02 20:09:15 +0100

    Commit: Thu 02 Nov 2023 08:09:15 PM CET

--- oor_stream_2023_11_02_code.r
+++ oor_stream_2023_11_02_code.r
@@ -101,5 +101,7 @@ predict(res, newdata=data.frame(hp=seq(60,200,by=20), am=1), interval="confidenc
 res0 <- lm(mpg ~ hp, data=mtcars)
 res1 <- lm(mpg ~ hp + am, data=mtcars)
 
+# model comparison
+anova(res0, res1)
 
 ############################################################################
