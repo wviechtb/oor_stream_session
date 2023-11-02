ed6fdd3e527276dc2fa99f81b7bbc2126e0d6173
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-11-02 18:14:55 +0100

    Commit: Thu 02 Nov 2023 06:14:55 PM CET

--- oor_stream_2023_11_02_code.r
+++ oor_stream_2023_11_02_code.r
@@ -16,4 +16,12 @@
 
 ## 11.3 Generic functions for extracting model information
 
+# inspect the mtcars dataset again
+mtcars
+
+# fit a linear regression model predicting miles per gallon from horsepower
+# and whether it is an automatic or manual transmission
+res <- lm(mpg ~ hp + am, data=mtcars)
+summary(res)
+
 ############################################################################
