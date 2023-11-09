9f7435adaa3254c4d5e45bcd66d1456bb2867d9b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-11-09 17:56:29 +0100

    Commit: Thu 09 Nov 2023 05:56:29 PM CET

---
+++ oor_stream_2023_11_09_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-11-09
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 4.4 - ?
+#
+# last updated: 2023-11-09
+
+############################################################################
+
+### 4.4: Statistical significance, hypothesis testing, and statistical errors
+
