16e86ab691eac19f377908de1f92a9f69f4f7bb8
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-11-09 19:38:30 +0100

    Commit: Thu 09 Nov 2023 07:38:30 PM CET

--- oor_stream_2023_11_09_code.r
+++ oor_stream_2023_11_09_code.r
@@ -15,3 +15,18 @@
 
 ### 4.4: Statistical significance, hypothesis testing, and statistical errors
 
+## Statistical significance
+
+# say we flip a coin 20 times and it is a fair coin (so 50% chance for heads
+# and 50% chance for tails); then we can construct the sampling distribution
+# of the proportion of heads observed in the 20 flips through simulation
+props <- rbinom(100000, size=20, prob=0.5) / 20
+
+# frequency table of the observed proportions
+tab <- table(props)
+tab
+
+# examine the sampling distribution of the proportions
+plot(tab, type="h", lwd=3, bty="l", xlab="Proportion", ylab="Frequency",
+     main="Sampling Distribution of the Proportion")
+
