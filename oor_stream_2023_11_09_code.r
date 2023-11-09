baf50ecf3843670c1ae55a825fd916d6dfa74089
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-11-09 19:56:34 +0100

    Commit: Thu 09 Nov 2023 07:56:34 PM CET

--- oor_stream_2023_11_09_code.r
+++ oor_stream_2023_11_09_code.r
@@ -46,6 +46,14 @@ sum(tab[props >= 0.7])
 # compute the chances of observing a proportion of 0.75 or higher
 sum(tab[props >= 0.75])
 
+# compute the chances of observing a proportion of 0.25 or lower
+sum(tab[props <= 0.25])
+
+# now imagine you do the experiment (flipping the coin 20 times and observing
+# the proportion of heads) once and you get the following result
+heads <- c(T, F, F, F, F, T, F, F, T, T, F, F, T, F, T, T, F, F, T, F)
+length(heads)
+mean(heads)
 
 ############################################################################
 
