691952f144815d4c5bc9aba871e57719dff466ad
Wolfgang Viechtbauer <wvb@wvbauer.com>
2023-11-30 17:50:31 +0100

    Commit: Thu 30 Nov 2023 05:50:31 PM CET

---
+++ oor_stream_2023_11_30_code.r
@@ -0,0 +1,23 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2023-11-30
+#
+# Topic(s):
+# - Drawing forest plots with the metafor package
+# - https://www.metafor-project.org
+#
+# last updated: 2023-11-30
+
+############################################################################
+
+# install the metafor package
+#install.packages("metafor")
+
+# load the metafor package
+library(metafor)
+
+############################################################################
+
