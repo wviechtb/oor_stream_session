d4b8608fedeb3d0ed2e0ae849e430643df7d6b50
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-01-18 21:57:28 +0100

    Commit: Thu 18 Jan 2024 09:57:28 PM CET

--- oor_stream_2023_11_30_code.r
+++ oor_stream_2023_11_30_code.r
@@ -309,6 +309,13 @@ forest(res, header=TRUE, mlab=expression("Q(df=12) = 152.23, p < .001;" ~ I^2 ==
 
 # instead of copy-pasting values from the output, we can automate the
 # extraction of the relevant pieces of information from the model object
+forest(res, header=TRUE, mlab=paste0("Q(df=", res$k-1, ") = ", fmtx(res$QE, digits=2),
+                                     fmtp(res$QEp, digits=3, pname="p", sep=TRUE),
+                                     "I^2 = ", fmtx(res$I2, digits=1),
+                                     "tau^2 = ", fmtx(res$tau2, digits=2)
+
+
+
 
 ### a little helper function to add Q-test, I^2, and tau^2 estimate info
 mlabfun <- function(text, x) {
