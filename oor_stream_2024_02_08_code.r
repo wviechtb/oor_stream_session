a3669129d1f22d00ef076c41a246225ff360fe9f
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-08 19:36:31 +0100

    Commit: Thu 08 Feb 2024 07:36:31 PM CET

--- oor_stream_2024_02_08_code.r
+++ oor_stream_2024_02_08_code.r
@@ -37,6 +37,23 @@ summary(res)
 dat$highmpg <- ifelse(dat$mpg > median(dat$mpg), 1, 0)
 dat
 
+res <- lm(highmpg ~ hp + wt + am, data=dat)
+summary(res)
+
+# fit a logistic regression model predicting highmpg from the same predictors
+# as above
+
+res <- glm(highmpg ~ hp + wt + am, family=binomial, data=dat)
+
+# the dependent variable here is a 0/1 variable, which is assumed to have a
+# binomial distribution, which, as a special case, is actually a Bernoulli
+# distribution (https://en.wikipedia.org/wiki/Bernoulli_distribution); the
+# mean of such a distribution is p, where p is the probability of seeing a 1;
+# the model says that m^{-1}(p) = beta0 + beta1*x1 + beta2*x2 + ..., but what
+# is this function m() and its inverse m^{-1}()? by default, it is the logit
+# function, which is given by log(p/(1-p)), so the model says that the
+# logit-transformed probability of a 1 is a linear function of one or multiple
+# predictors
 
 
 ############################################################################
