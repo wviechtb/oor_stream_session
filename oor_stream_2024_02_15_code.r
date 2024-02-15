a8ba38ae1e1c817acfd703b38b4420482078c860
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-15 19:19:39 +0100

    Commit: Thu 15 Feb 2024 07:19:39 PM CET

--- oor_stream_2024_02_15_code.r
+++ oor_stream_2024_02_15_code.r
@@ -22,7 +22,25 @@
 dat <- read.csv("riverbay.csv", header=FALSE)
 dat
 
-names(dat) <- c("candidate", paste0("tally", 1:6), "
+# remove the first column
+dat <- dat[-1]
 
+# give proper names to the variables in the dataset
+names(dat) <- c(paste0("tally", 1:6), "candidate")
+
+# number of voters at each tally (note: each voter could vote for up to 6
+# candidates, so the total counts at each tally can be as high as 6 times the
+# number of voters)
+voters <- c(600, 1200, 2444, 3444, 4444, 5553)
+
+# proportion of votes received at the very end
+dat$propend <- dat$tally6 / voters[6]
+
+# sort the dataset by proportion of votes received (in decreasing order, so
+# the first row is the person who received the most votes)
+dat <- dat[order(dat$propend, decreasing=TRUE),]
+
+# reset the row names to increasing integers
+rownames(dat) <- NULL
 
 
