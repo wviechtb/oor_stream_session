8217deb11ecc0e9545dfa6471fab9460d63091ba
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-22 18:29:02 +0100

    Commit: Thu 22 Feb 2024 06:29:02 PM CET

--- oor_stream_2024_02_22_code.r
+++ oor_stream_2024_02_22_code.r
@@ -16,3 +16,45 @@
 
 ## 11.7: Nonlinear least squares and maximum likelihood models
 
+# 11.7.1: Least squares
+
+# example data from Bates & Watts (1988), page 51
+x <- c(0.02, 0.02, 0.06, 0.06, 0.11, 0.11, 0.22, 0.22, 0.56, 0.56, 1.10, 1.10)
+y <- c(76, 47, 97, 107, 123, 139, 159, 152, 191, 201, 207, 200)
+
+# scatterplot of x versus y
+plot(x, y, pch=21, bg="gray", cex=1.2, ylim=c(40,220))
+
+# naive fit a simple linear regression model
+res1 <- lm(y ~ x)
+summary(res1)
+
+# compute predicted values based on the model for values of x between 0 and 1.2
+xs <- seq(0, 1.2, length=100)
+pred <- predict(res1, newdata=data.frame(x=xs))
+
+# add the regression line based on the predicted values to the plot
+lines(xs, pred, lwd=3, col="black")
+
+# fit a quadratic polynomial regression model
+res2 <- lm(y ~ x + I(x^2))
+summary(res2)
+
+# compute predicted values based on the model for values of x between 0 and 1.2
+xs <- seq(0, 1.2, length=100)
+pred <- predict(res2, newdata=data.frame(x=xs))
+
+# add the regression line based on the predicted values to the plot
+lines(xs, pred, lwd=3, col="blue")
+
+# fit a cubic polynomial regression model
+res3 <- lm(y ~ x + I(x^2) + I(x^3))
+summary(res3)
+
+# compute predicted values based on the model for values of x between 0 and 1.2
+xs <- seq(0, 1.2, length=100)
+pred <- predict(res3, newdata=data.frame(x=xs))
+
+# add the regression line based on the predicted values to the plot
+lines(xs, pred, lwd=3, col="red")
+
