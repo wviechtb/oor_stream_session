e2d14a7b4d4569259b02b11385d88724d7d738a8
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-22 17:53:00 +0100

    Commit: Thu 22 Feb 2024 05:53:00 PM CET

---
+++ oor_stream_2024_02_22_code.r
@@ -0,0 +1,18 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-02-22
+#
+# Topic(s):
+# - An Introduction to R
+#   https://cran.r-project.org/doc/manuals/r-release/R-intro.html
+# - Section(s): 11.6
+#
+# last updated: 2024-02-22
+
+############################################################################
+
+## 11.7: Nonlinear least squares and maximum likelihood models
+
