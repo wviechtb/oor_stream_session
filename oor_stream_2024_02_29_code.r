c818f1a7db8b95be500af737916eaf6b5a54c30b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-29 17:37:58 +0100

    Commit: Thu 29 Feb 2024 05:37:58 PM CET

---
+++ oor_stream_2024_02_29_code.r
@@ -0,0 +1,18 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-02-29
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 5.1 - ?
+#
+# last updated: 2024-02-29
+
+############################################################################
+
+### 5.1: Simulation of discrete probability models
+
+############################################################################
