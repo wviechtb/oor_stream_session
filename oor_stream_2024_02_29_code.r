7c051d4abb82ab5c1039267246a9ebd83b308f1e
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-29 18:22:00 +0100

    Commit: Thu 29 Feb 2024 06:22:00 PM CET

--- oor_stream_2024_02_29_code.r
+++ oor_stream_2024_02_29_code.r
@@ -15,4 +15,17 @@
 
 ### 5.1: Simulation of discrete probability models
 
+# simulate a single draw from a binomial distribution with 400 'trials'
+# (births) and a probability of .488 that the event of interest occurs on a
+# single trial (i.e., that the baby is a girl)
+n_girls <- rbinom(1, 400, 0.488)
+n_girls
+
+# repeat this process 1000 times and score the simulated values in a vector
+n_sims <- 1000
+n_girls <- rep(NA, n_sims)
+for (i in 1:n_sims) {
+  n_girls[i] <- rbinom(1, 400, 0.488)
+}
+
 ############################################################################
