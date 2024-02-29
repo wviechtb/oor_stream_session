6f98fc6233b1bb9de61411abe5083f805dc47997
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-02-29 18:36:03 +0100

    Commit: Thu 29 Feb 2024 06:36:03 PM CET

--- oor_stream_2024_02_29_code.r
+++ oor_stream_2024_02_29_code.r
@@ -18,27 +18,46 @@
 # simulate a single draw from a binomial distribution with 400 'trials'
 # (births) and a probability of .488 that the event of interest occurs on a
 # single trial (i.e., that the baby is a girl)
-n_girls <- rbinom(1, 400, 0.488)
-n_girls
+girls <- rbinom(1, 400, 0.488)
+girls
 
 # repeat this process 1000 times and score the simulated values in a vector
-n_sims <- 1000
-n_girls <- rep(NA, n_sims)
-for (i in 1:n_sims) {
-   n_girls[i] <- rbinom(1, 400, 0.488)
+sims <- 1000
+girls <- rep(NA, sims)
+for (i in 1:sims) {
+   girls[i] <- rbinom(1, 400, 0.488)
 }
 
 # create a histogram of the simulated values
-hist(n_girls, main="", xlab="Number of Girls (out of 400)")
+hist(girls, main="", xlab="Number of Girls (out of 400)")
 
 # we don't really need a for-loop to do the above; we can directly simulate
 # 1000 values from the binomial distribution; to make the simulated values
 # reproducible, we also set the seed of the random number generator
 set.seed(1234)
-n_girls <- rbinom(n_sims, 400, 0.488)
-n_girls
+girls <- rbinom(sims, 400, 0.488)
+girls
 
 # create a histogram of the simulated values
-hist(n_girls, main="", xlab="Number of Girls (out of 400)")
+hist(girls, main="", xlab="Number of Girls (out of 400)")
+
+#
+birth_type <- sample(c("fraternal twin","identical twin","single birth"),
+                     size=400, replace=TRUE, prob=c(1/125, 1/300, 1-1/125-1/300))
+girls <- rep(NA, 400)
+for (i in 1:400){
+  if (birth_type[i]=="single birth"){
+    girls[i] <- rbinom(1, 1, 0.488)}
+  else if (birth_type[i]=="identical twin"){
+    girls[i] <- 2*rbinom(1, 1, 0.495)}
+  else if (birth_type[i]=="fraternal twin"){
+    girls[i] <- rbinom(1, 2, 0.495)}
+}
+n_girls <- sum(girls)
+
+girls <- ifelse(birth_type=="single birth", rbinom(400, 1, 0.488),
+  ifelse(birth_type=="identical twins", 2*rbinom(400, 1, 0.495),
+  rbinom(400, 2, 0.495)))
+
 
 ############################################################################
