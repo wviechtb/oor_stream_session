c332166270d8b87fa441f1da481f9985f8d66353
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-03-28 18:42:05 +0100

    Commit: Thu 28 Mar 2024 06:42:05 PM CET

--- oor_stream_2024_03_28_code.r
+++ oor_stream_2024_03_28_code.r
@@ -16,6 +16,18 @@
 
 ## 12.4: Using graphics parameters
 
+# copy the mtcars dataset to dat
+dat <- mtcars
+dat
+
+# a default looking scatterplot
+plot(mpg ~ hp, data=dat)
+
+# change the plotting symbol to a filled circle and use gray as the background
+# color of the circles, use only an L-shape around the plot, and add proper
+# x-axis and y-axis labels
+plot(mpg ~ hp, data=dat, pch=21, bg="gray", bty="l", xlab="Horsepower",
+     ylab="Miles per Gallon")
 
 
 ############################################################################
