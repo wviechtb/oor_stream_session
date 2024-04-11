b38386c9416ebb88bca3f5d6304f79b69e8f5bee
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-04-11 18:20:45 +0200

    Commit: Thu 11 Apr 2024 06:20:45 PM CEST

--- oor_stream_2024_04_11_code.r
+++ oor_stream_2024_04_11_code.r
@@ -15,11 +15,27 @@
 
 ### 6.5: The paradox of regression to the mean
 
+# install the rstanarm package (need to do this once)
+#install.packages("rstanarm")
+
+# load the rstanarm package
+library(rstanarm)
+
 # download the dataset (only need to do this once)
 #download.file("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/PearsonLee/data/Heights.txt", destfile="heights.txt")
 
 # read in the data
 dat <- read.table("heights.txt", header=TRUE)
 
+# inspect the dataset
+head(dat)
+
+# fit a simple linear regression model predicting the daughter's height based
+# on the mother's height
+res <- stan_glm(daughter_height ~ mother_height, data=dat)
+res
+
+# extract the coefficients (rounded to two decimal places)
+round(coef(res), digits=2)
 
 ############################################################################
