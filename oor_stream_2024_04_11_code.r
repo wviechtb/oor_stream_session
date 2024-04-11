7a82d224e2987601f9b6d044adb3ab85663cdb4e
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-04-11 17:34:43 +0200

    Commit: Thu 11 Apr 2024 05:34:43 PM CEST

---
+++ oor_stream_2024_04_11_code.r
@@ -0,0 +1,16 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-04-11
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 6.5 - ?
+#
+# last updated: 2024-04-11
+
+############################################################################
+
+############################################################################
