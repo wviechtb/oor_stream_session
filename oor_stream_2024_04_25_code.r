8ccbed7caa502c7792949333b94389d004d1d1a2
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-04-25 17:28:42 +0200

    Commit: Thu 25 Apr 2024 05:28:42 PM CEST

---
+++ oor_stream_2024_04_25_code.r
@@ -0,0 +1,22 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-04-25
+#
+# Topic(s):
+# - An Introduction to R
+#   https://cran.r-project.org/doc/manuals/r-release/R-intro.html
+# - Section(s): 13.1 - ?
+#
+# last updated: 2024-04-25
+
+############################################################################
+
+### 13: Packages
+
+############################################################################
+
+## 13.1: Standard packages
+
