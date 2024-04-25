c8263c389d6f32d54e268547c2d276dda18c3958
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-04-25 18:12:45 +0200

    Commit: Thu 25 Apr 2024 06:12:45 PM CEST

--- oor_stream_2024_04_25_code.r
+++ oor_stream_2024_04_25_code.r
@@ -16,6 +16,33 @@
 
 ### 13: Packages
 
+# see which packages are installed
+library()
+
+# terminology:
+# - package = book
+# - library = place where you store books
+# (don't say you are using the 'xyz library' for your analysis!)
+
+
+# another way
+
+installed.packages()
+
+# only show where installed, version, and the 'priority'
+
+installed.packages()[,c("LibPath", "Version", "Priority")]
+
+# the 'base' and 'recommended' packages (see 'Priority' column) are installed
+# with R automatically; it is also possible to install an updated version of
+# recommended packages (if there is an update)
+
+# in RStudio, there is also the 'Packages' pane (bottom-right)
+
+# list loaded packages (have to explicitly use print())
+
+print(.packages())
+
 
 
 # name clashes (masking)
