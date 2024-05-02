495d64ef0386b29f19734fe0291bf4cb39620fd1
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-05-02 17:56:47 +0200

    Commit: Thu 02 May 2024 05:56:47 PM CEST

---
+++ oor_stream_2024_05_02_code.r
@@ -0,0 +1,27 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-05-02
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 8.1 - ?
+#
+# last updated: 2024-05-02
+
+############################################################################
+
+# for the survey about topics for future streams (alternating with the coverage
+# of the present book), go here: https://forms.gle/VEEKNRvquRCGKjb78
+
+############################################################################
+
+# - security vulnerability in older version of R
+# - sort_by() function
+
+############################################################################
+
+### 8.1: Least squares, maximum likelihood, and Bayesian inference
+
