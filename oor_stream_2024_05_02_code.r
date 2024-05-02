98b40302c4bff713ebe86907a18d576e07135ffa
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-05-02 21:57:35 +0200

    Commit: Thu 02 May 2024 09:57:35 PM CEST

--- oor_stream_2024_05_02_code.r
+++ oor_stream_2024_05_02_code.r
@@ -233,7 +233,7 @@ filled.contour(as, bs, ll, color.palette=hcl.colors,
                   axis(side=1)
                   axis(side=2)
                   segments(b, a-se[1], b, a+se[1], lwd=2, col="red")
-                  segments(a, b-se[2], a, b+se[2], lwd=2, col="red")
+                  #segments(a, b-se[2], a, b+se[2], lwd=2, col="red")
                   points(a, b, pch=19, col="red")
                })
 
