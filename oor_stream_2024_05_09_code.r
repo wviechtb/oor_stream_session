033d7adfac8f374aba7f87748460cb5f10d5bea9
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-05-09 17:55:56 +0200

    Commit: Thu 09 May 2024 05:55:56 PM CEST

---
+++ oor_stream_2024_05_09_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-05-09
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 8.1
+#
+# last updated: 2024-05-09
+
+############################################################################
+
+### 8.1: Least squares, maximum likelihood, and Bayesian inference
+
