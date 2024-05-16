0e104015fae0cca37606435e992ddc616f1df39b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-05-16 17:32:55 +0200

    Commit: Thu 16 May 2024 05:32:55 PM CEST

---
+++ oor_stream_2024_05_16_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-05-16
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 9.1 - ?
+#
+# last updated: 2024-05-16
+
+############################################################################
+
+### 9.1: Propagating uncertainty in inference using posterior simulations
+
