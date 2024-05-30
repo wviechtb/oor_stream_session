b8657eeeaa7d43aff02290b9b3eefd13279a6214
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-05-30 18:42:24 +0200

    Commit: Thu 30 May 2024 06:42:24 PM CEST

--- oor_stream_2024_05_30_code.r
+++ oor_stream_2024_05_30_code.r
@@ -20,15 +20,21 @@ dat <- structure(list(x = c(-2, -1, 0, 1, 2), y = c(50, 44, 50, 47, 56)),
                  row.names = c(NA, -5L), class = "data.frame")
 dat
 
-# proportion of girls of parents in the lower attactiveness categories versus
+# percentage of girls of parents in the lower attactiveness categories versus
 # the highest attractiveness category
 p1 <- mean(dat$y[1:4])
 p2 <- dat$y[5]
 p1
 p2
 
+# difference between the two percentages
+p2 - p1
+
 # assume that 4/5th of the respondents are in the first group and the other
 # 1/5th in the other group
 n1 <- 4/5 * 3000
 n2 <- 1/5 * 3000
 
+# standard error of the difference between the two percentages (in the book,
+# this is given as 3 percentage points)
+sqrt(p1 * (100 - p1) / n1 + p2 * (100 - p2) / n2)
