52e8c67822eb1d39a24a15a1c5bb08ebae8907b1
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-05-30 17:14:16 +0200

    Commit: Thu 30 May 2024 05:14:16 PM CEST

---
+++ oor_stream_2024_05_30_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-05-30
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 9.4 - ?
+#
+# last updated: 2024-05-30
+
+############################################################################
+
+### 9.4: Example of Bayesian inference: beauty and sex ratio
+
