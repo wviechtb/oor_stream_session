6b49955158bca0c9461ca645ed3055b1ecc3a7d0
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-06-06 19:22:29 +0200

    Commit: Thu 06 Jun 2024 07:22:29 PM CEST

--- oor_stream_2024_06_06_code.r
+++ oor_stream_2024_06_06_code.r
@@ -13,22 +13,35 @@
 
 ############################################################################
 
+### 2.1: The garden of forking data
+
+# the observed marbles
 obs <- c("B","W","B")
 
-conj <- expand.grid(replicate(3, c("B","W","W","W"), simplify=FALSE))
-conj[conj[,1] == obs[1] & conj[,2] == obs[2] & conj[,3] == obs[3],]
+# write out the possible paths for each conjecture
+conj1 <- expand.grid(replicate(3, c("W","W","W","W"), simplify=FALSE))
+conj2 <- expand.grid(replicate(3, c("B","W","W","W"), simplify=FALSE))
+conj3 <- expand.grid(replicate(3, c("B","B","W","W"), simplify=FALSE))
+conj4 <- expand.grid(replicate(3, c("B","B","W","W"), simplify=FALSE))
+conj5 <- expand.grid(replicate(3, c("B","B","B","W"), simplify=FALSE))
+conj6 <- expand.grid(replicate(3, c("B","B","B","B"), simplify=FALSE))
 
-conj <- expand.grid(replicate(3, c("B","B","W","W"), simplify=FALSE))
-conj[conj[,1] == obs[1] & conj[,2] == obs[2] & conj[,3] == obs[3],]
+# examine the one for the second conjecture
+conj2
 
-conj <- expand.grid(replicate(3, c("B","B","W","W"), simplify=FALSE))
-conj <- expand.grid(replicate(3, c("B","B","B","W"), simplify=FALSE))
+# compute the number of paths in each conjecture that match the data
+sum(rowSums(obs == conj1) == 3)
+sum(rowSums(obs == conj2) == 3)
+sum(rowSums(obs == conj3) == 3)
+sum(rowSums(obs == conj4) == 3)
+sum(rowSums(obs == conj5) == 3)
 
-sel <- conj
-for (i in 1:3) {
-   sel <- sel[sel[,i] == obs[i],]
-}
-sel
+# now we draw another marble and it is blue
+obs <- c("B","W","B","B")
 
-sum(rowSums(obs == conj) == 3)
-
+# compute the number of paths in each conjecture that match the data
+sum(rowSums(obs == conj1) == 3)
+sum(rowSums(obs == conj2) == 3)
+sum(rowSums(obs == conj3) == 3)
+sum(rowSums(obs == conj4) == 3)
+sum(rowSums(obs == conj5) == 3)
