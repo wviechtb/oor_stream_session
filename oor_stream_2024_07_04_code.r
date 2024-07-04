4d9959f26269190f09f150c530c11e47858b53d7
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-07-04 21:00:22 +0200

    Commit: Thu 04 Jul 2024 09:00:22 PM CEST

--- oor_stream_2024_07_04_code.r
+++ oor_stream_2024_07_04_code.r
@@ -207,3 +207,22 @@ sqrt(-1/res2$hessian[1,1])
 
 res1$sd
 
+## 2.4.5: Markov chain Monte Carlo
+
+n_samples <- 1000
+p <- rep(NA , n_samples)
+p[1] <- 0.5
+W <- 6
+L <- 3
+for (i in 2:n_samples) {
+   p_new <- rnorm(1, p[i-1], 0.1)
+   if (p_new < 0) p_new <- abs(p_new)
+   if (p_new > 1) p_new <- 2 - p_new
+   q0 <- dbinom(W, W+L, p[i-1])
+   q1 <- dbinom(W, W+L, p_new)
+   p[i] <- ifelse(runif(1) < q1/q0, p_new, p[i-1])
+}
+
+## R code 2.9
+dens( p , xlim=c(0,1) )
+curve( dbeta( x , W+1 , L+1 ) , lty=2 , add=TRUE )
