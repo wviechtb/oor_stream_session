6da4fbcde328c40b9fb66ee896f777d8868399f8
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-08-22 17:58:00 +0200

    Commit: Thu 22 Aug 2024 05:58:00 PM CEST

---
+++ oor_stream_2024_08_22_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-08-22
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 10.1 - ?
+#
+# last updated: 2024-08-22
+
+############################################################################
+
+### 10.1: Adding predictors to a model
+
