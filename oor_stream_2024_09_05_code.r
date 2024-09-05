99309b65037b78b1d11c49aa26aa4119210fc48c
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-05 20:19:35 +0200

    Commit: Thu 05 Sep 2024 08:19:35 PM CEST

--- oor_stream_2024_09_05_code.r
+++ oor_stream_2024_09_05_code.r
@@ -103,4 +103,11 @@ sum(samples < 0.5) / 1e4
 # compute the proportion of samples values that are above 0.5 and below 0.75
 sum(samples > 0.5 & samples < 0.75) / 1e4
 
+# Figure 3.2
+par(mfrow=c(2,2))
+plot(p_grid, posterior, type="l", xlab="proportion water (p)", ylab="Density")
+sel <- p_grid < 0.5
+polygon(c(p_grid[sel], rev(p_grid[sel])), c(posterior[sel], rev(posterior[sel])), col="dodgerblue")
+
+
 ############################################################################
