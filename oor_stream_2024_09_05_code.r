fd50fd326a2fce920132d16e80acda1c4772ec9b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-05 18:31:07 +0200

    Commit: Thu 05 Sep 2024 06:31:07 PM CEST

--- oor_stream_2024_09_05_code.r
+++ oor_stream_2024_09_05_code.r
@@ -13,6 +13,21 @@
 
 ############################################################################
 
+# code for the vampire test example
+
+Pr_Positive_Vampire <- 0.95 # also called the sensitivity of the test
+Pr_Positive_Mortal  <- 0.01 # 1-this is called the specificity of the test (0.99)
+Pr_Vampire <- 0.001
+Pr_Positive <- Pr_Positive_Vampire * Pr_Vampire +
+               Pr_Positive_Mortal  * (1 - Pr_Vampire)
+Pr_Vampire_Positive <- Pr_Positive_Vampire * Pr_Vampire / Pr_Positive
+Pr_Vampire_Positive
+
+
+
+
 ### 3.1: Sampling from a grid-approximate posterior
+
+
 
 ############################################################################
