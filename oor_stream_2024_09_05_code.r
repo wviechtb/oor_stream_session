fc1a8f3e5329c5e00a5ed4f6ad5062db3dc5fe52
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-05 19:57:28 +0200

    Commit: Thu 05 Sep 2024 07:57:28 PM CEST

--- oor_stream_2024_09_05_code.r
+++ oor_stream_2024_09_05_code.r
@@ -63,7 +63,7 @@ prob_p <- rep(1, 1000) # prior
 prob_data <- dbinom(6, size=9, prob=p_grid) # likelihood
 plot(p_grid, prob_data, type="l") # plot the likelihood function
 posterior <- prob_data * prob_p
-#posterior <- posterior / sum(posterior)
+posterior <- posterior / sum(posterior)
 plot(p_grid, posterior, type="l") # plot the posterior distribution
 
 # sample values of p in accordance with how probable the values are (which we
@@ -75,8 +75,14 @@ par(mfrow=c(1,2))
 plot(samples, pch=19, xlab="sample number",
      ylab="proportion water (p)", col=rgb(0,0,0.5,0.1))
 dens(samples, lwd=3, col="blue", xlab="proportion water (p)")
-lines(p_grid, posterior) # superimpose the posterior grid approximation
+
+# superimpose the posterior grid approximation (note: we have to rescale the
+# posterior values so that the area of the p
+lines(p_grid, 999*posterior)
+par(mfrow=c(1,1))
 
 plot(samples, pch=19, cex=0.1, type="o")
+
+sum(diff(p_grid) * posterior[-1])
 
 ############################################################################
