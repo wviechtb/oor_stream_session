888f2d95cf6c3a22815fd4548e908cc7371afab9
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-05 19:31:17 +0200

    Commit: Thu 05 Sep 2024 07:31:17 PM CEST

--- oor_stream_2024_09_05_code.r
+++ oor_stream_2024_09_05_code.r
@@ -55,7 +55,13 @@ Pr_Vampire_Positive
 
 ### 3.1: Sampling from a grid-approximate posterior
 
-
+p_grid <- seq(from=0, to=1, length.out=1000)
+prob_p <- rep(1, 1000) # prior
+prob_data <- dbinom(6, size=9, prob=p_grid) # likelihood
+plot(p_grid, prob_data, type="l") # plot the likelihood function
+posterior <- prob_data * prob_p
+posterior <- posterior / sum(posterior)
+plot(p_grid, posterior, type="l") # plot the posterior distribution
 
 
 ############################################################################
