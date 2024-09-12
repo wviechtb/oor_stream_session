038cc456e01bf822684f8c6a103e3cc1f414b32a
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-12 18:43:45 +0200

    Commit: Thu 12 Sep 2024 06:43:45 PM CEST

--- oor_stream_2024_09_12_code.r
+++ oor_stream_2024_09_12_code.r
@@ -26,15 +26,18 @@ head(dat)
 # shares (i.e., change the proportions to essentially 0 or 1, respectively)
 dat$v88_hist <- ifelse(dat$v88<.1, .0001, ifelse(dat$v88>.9, .9999, dat$v88))
 
-# Figure 10.5: Proportion of the vote share for the Democratic party in the
+# Figure 10.5: proportion of the vote share for the Democratic party in the
 # 435 congressional districts in 1988 (using the v88_hist variable)
 hist(dat$v88_hist, breaks=seq(0,1,by=.05), main="",
      xlab="Democratic share of the two-party vote")
 
-
-
+# Figure 10.6a: proportion of the vote share in 1988 versus 1986 with filled
+# circles when the incumbent is from the Democratic party, crosses when the
+# incumbent is from the Republican party, and unfilled circles otherwise
 plot(jitter(dat$v86, amount=.01), jitter(dat$v88, amount=.01),
-     pch=c(4,1,19)[as.numeric(factor(dat$inc88))])
+     pch=c(4,1,19)[as.numeric(factor(dat$inc88))],
+     xlab="Democratic vote share in 1986", ylab="Democratic vote share in 1988",
+     panel.first=abline(0,1))
 
 # load the rstanarm package
 library(rstanarm)
