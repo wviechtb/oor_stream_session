143a948d4d31e2e0db0d96fd6393c979c50db140
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-12 17:13:38 +0200

    Commit: Thu 12 Sep 2024 05:13:38 PM CEST

---
+++ oor_stream_2024_09_12_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-09-12
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 10.6 - ?
+#
+# last updated: 2024-09-12
+
+############################################################################
+
+### 10.6: Example: uncertainty in predicting congressional elections
+
