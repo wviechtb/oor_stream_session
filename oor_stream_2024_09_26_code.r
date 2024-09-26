2428bd0c44929f705d00d143f453f864f9a06906
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-09-26 17:14:54 +0200

    Commit: Thu 26 Sep 2024 05:14:54 PM CEST

---
+++ oor_stream_2024_09_26_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-09-26
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 11.1 - ?
+#
+# last updated: 2024-09-26
+
+############################################################################
+
+### 11.1: Assumptions of regression analysis
+
