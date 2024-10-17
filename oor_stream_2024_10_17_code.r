8a924aed2171caaf990c61b938fba1084f9c1f9e
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-10-17 17:36:36 +0200

    Commit: Thu 17 Oct 2024 05:36:36 PM CEST

---
+++ oor_stream_2024_10_17_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-10-17
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 11.4 - ?
+#
+# last updated: 2024-10-17
+
+############################################################################
+
+### 11.4: Comparing data to replications from a fitted model
+
