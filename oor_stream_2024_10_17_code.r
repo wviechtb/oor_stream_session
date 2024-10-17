74780aea3dab0ee0b0b25cf2ccbb8787ae6817c3
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-10-17 18:46:41 +0200

    Commit: Thu 17 Oct 2024 06:46:41 PM CEST

--- oor_stream_2024_10_17_code.r
+++ oor_stream_2024_10_17_code.r
@@ -30,6 +30,7 @@ y <- 1 + 4*x + -2*x^2 + rnorm(N, mean=0, sd=0.25)
 dat <- data.frame(x=x, y=y)
 head(dat)
 
+# plot x versus y
 plot(dat$x, dat$y, xlab="x", ylab="y", pch=21, bg="darkgray")
 
 # fit the model that assumes a linear relationship between x and y
@@ -48,6 +49,32 @@ abline(h=0, col="gray", lwd=5)
 # we see an upside down U shape in the plot, which is indicative of missing a
 # non-linear relationship
 
+# simulate data with heteroscedasticity
+set.seed(1234)
+N <- 500
+x <- runif(N, 0, 1)
+y <- 1 + 1*x + rnorm(N, mean=0, sd=(0.1 + 0.5*(1 - x))
+dat <- data.frame(x=x, y=y)
+head(dat)
+
+# plot x versus y
+plot(dat$x, dat$y, xlab="x", ylab="y", pch=21, bg="darkgray")
+
+# fit the model that assumes a linear relationship between x and y
+res <- stan_glm(y ~ x, data=dat, refresh=0)
+res
+
+# compute the predicted values and residuals
+pred  <- predict(res)
+resid <- dat$y - pred
+
+# plot the fitted values versus the residuals
+plot(pred, resid, pch=19, main="Residuals vs. predicted values",
+     xlab="predicted value", ylab="residual")
+abline(h=0, col="gray", lwd=5)
+
+# we see an upside down U shape in the plot, which is indicative of missing a
+# non-linear relationship
 
 
 ### 11.4: Comparing data to replications from a fitted model
