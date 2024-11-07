eb12973ff5c9f3fcb52673c2c04441c7c1ca155b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-11-07 21:55:20 +0100

    Commit: Thu 07 Nov 2024 09:55:20 PM CET

--- oor_stream_2024_11_07_code.r
+++ oor_stream_2024_11_07_code.r
@@ -323,9 +323,11 @@ round(ncp.hi / (ncp.hi + p-1 + n-p + 1), digits=2)
 ncps <- seq(30, 300, length=1000)
 denFval <- sapply(ncps, function(ncp) df(Fval, df1=2, df2=431, ncp=ncp))
 R2s <- ncps / (ncps + p-1 + n-p + 1)
-
 trapezoid <- function(x,y) sum(diff(x)*(y[-1]+y[-length(y)]))/2
-trapezoid(R2s, denFval)
+denFval <- denFval / trapezoid(R2s, denFval)
 
+# the histogram of the Bayesian R^2 values and the distribution obtained this
+# way matches up very closely
 hist(bayes_R2(res2), main="", breaks=30, xlab="Bayesian R^2", freq=FALSE)
-lines(R2s, denFval/trapezoid(R2s, denFval), type="l", lwd=5)
+lines(R2s, denFval, type="l", lwd=5)
+
