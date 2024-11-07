5c09b7164455312842493d1790f0cf61c20a6a8a
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-11-07 18:32:20 +0100

    Commit: Thu 07 Nov 2024 06:32:20 PM CET

--- oor_stream_2024_11_07_code.r
+++ oor_stream_2024_11_07_code.r
@@ -18,5 +18,20 @@ library(rstanarm)
 
 ############################################################################
 
-### 11.6: Residual standard deviation 𝜎 and explained variance R^2
+### 11.6: Residual standard deviation sigma and explained variance R^2
+
+# download the dataset (need to do this once)
+if (!file.exists("kidiq.csv")) download.file("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/KidIQ/data/kidiq.csv", destfile="kidiq.csv")
+
+# read in the data and inspect the first 6 rows
+dat <- read.csv("kidiq.csv")
+head(dat)
+
+# fit a linear regression model predicting the kids' test score from the moms' IQ
+res <- stan_glm(kid_score ~ mom_iq, data=dat, refresh=0)
+res
+
+# add mom_hs (0/1 for whether a mom completed high-school) as a predictor
+res <- stan_glm(kid_score ~ mom_hs + mom_iq, data=dat, refresh=0)
+res
 
