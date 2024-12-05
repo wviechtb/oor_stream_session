2de896c2236b794ec01c650e2cd91927a921b008
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-12-05 17:35:11 +0100

    Commit: Thu 05 Dec 2024 05:35:11 PM CET

---
+++ oor_stream_2024_12_05_code.r
@@ -0,0 +1,16 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-12-05
+#
+# Topic(s):
+# - Statistical Rethinking (https://xcelab.net/rm/)
+# - Section(s): 4.4 - ?
+#
+# last updated: 2024-12-05
+
+############################################################################
+
+### 4.4: Linear prediction
