3bd22ff57bd3803298e0e70190aa803e05507cfd
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-12-12 19:21:55 +0100

    Commit: Thu 12 Dec 2024 07:21:55 PM CET

--- oor_stream_2024_12_12_code.r
+++ oor_stream_2024_12_12_code.r
@@ -15,3 +15,20 @@
 
 ### 11.7: External validation: checking fitted model on new data
 
+# load the rstanarm package
+library(rstanarm)
+
+# download the dataset if it doesn't already exist
+if (!file.exists("kidiq.csv")) download.file("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/KidIQ/data/kidiq.csv", destfile="kidiq.csv")
+
+# read in the data and inspect the first 6 rows
+dat <- read.csv("kidiq.csv")
+head(dat)
+
+# the dataset doesn't actually include information about the age of the
+# children, so we cannot replicate exactly what is described in the book;
+# however, let's pretend that the first 98 rows of the dataset contains the
+# data of the older children, so we fit the model based on this subset
+res1 <- stan_glm(kid_score ~ mom_iq + mom_hs, data=dat, refresh=0, subset=1:98)
+
+
