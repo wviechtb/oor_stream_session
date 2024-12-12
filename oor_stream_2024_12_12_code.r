fe692c7223363d9da5d6b8c91e05d25b1fc6a661
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-12-12 17:29:53 +0100

    Commit: Thu 12 Dec 2024 05:29:53 PM CET

---
+++ oor_stream_2024_12_12_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2024-12-12
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 11.7
+#
+# last updated: 2024-12-12
+
+############################################################################
+
+### 11.7: External validation: checking fitted model on new data
+
