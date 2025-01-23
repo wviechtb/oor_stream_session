951922499293bb4785ca9e27dcea23568e01b7fd
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-01-23 17:55:48 +0100

    Commit: Thu 23 Jan 2025 05:55:48 PM CET

---
+++ oor_stream_2025_01_23_code.r
@@ -0,0 +1,23 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-01-23
+#
+# Topic(s):
+# - Statistical Rethinking (https://xcelab.net/rm/)
+# - Section(s): 4.5 - ?
+#
+# last updated: 2025-01-23
+
+############################################################################
+
+### 4.5: Curves from lines
+
+# load the rethinking package
+library(rethinking)
+
+# get the Howell1 data and put it into 'dat'
+dat <- get(data(Howell1))
+
