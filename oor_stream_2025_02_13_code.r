54a9628d243ca6e3e525be6e7d3c0c88fdc28a5a
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-02-13 17:34:01 +0100

    Commit: Thu 13 Feb 2025 05:34:01 PM CET

---
+++ oor_stream_2025_02_13_code.r
@@ -0,0 +1,24 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-02-13
+#
+# Topic(s):
+# - the 'chesstrainer' package
+# - interactive apps (without Shiny)
+#
+# last updated: 2025-02-13
+
+############################################################################
+
+### the chesstrainer package
+
+# https://github.com/wviechtb/chesstrainer
+# https://wviechtb.github.io/chesstrainer/
+
+# install the package
+install.packages("remotes")
+remotes::install_github("wviechtb/chesstrainer")
+
