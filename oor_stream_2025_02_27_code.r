9b7a4bf9d43605e58bfb3ce8a89a6173f7c23deb
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-02-27 19:19:21 +0100

    Commit: Thu 27 Feb 2025 07:19:21 PM CET

--- oor_stream_2025_02_27_code.r
+++ oor_stream_2025_02_27_code.r
@@ -80,16 +80,20 @@ mu <- link(res, data=list(A=A_seq))
 # we obtained above manually
 head(mu[,1])
 
-
 # compute percentile interval of mean
 mu.mean <- apply(mu, 2, mean)
-mu.PI <- apply(mu, 2, PI)
+mu.pi   <- apply(mu, 2, PI, prob=0.95)
 
 # plot it all
-plot(D ~ A, data=dat, col=rangi2)
+plot(D ~ A, data=dat, pch=21, bg="gray", xlab="Median age marriage (std)",
+     ylab="Divorce rate (std)", bty="l")
 lines(A_seq, mu.mean, lwd=2)
-shade(mu.PI, A_seq)
+shade(mu.pi, A_seq)
 
-
-
-
+# show the x- and y-values on the original scale
+plot(D ~ A, data=dat, pch=21, bg="gray", xlab="Median age marriage (std)",
+     ylab="Divorce rate (std)", bty="l", xaxt="n", yaxt="n")
+axis(side=1, at=((23:30) - mean(dat$MedianAgeMarriage)) / sd(dat$MedianAgeMarriage), labels=23:30)
+axis(side=2, at=((6:14) - mean(dat$Divorce)) / sd(dat$Divorce), labels=6:14)
+lines(A_seq, mu.mean, lwd=2)
+shade(mu.pi, A_seq)
