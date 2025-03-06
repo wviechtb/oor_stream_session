b402bd4417a648ae011e89dc422b848034fba5b1
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-03-06 17:44:46 +0100

    Commit: Thu 06 Mar 2025 05:44:46 PM CET

---
+++ oor_stream_2025_03_06_code.r
@@ -0,0 +1,20 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-03-06
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 12.5 - ?
+#
+# last updated: 2025-03-06
+
+############################################################################
+
+# load the rstanarm package
+library(rstanarm)
+
+### 12.5: Other transformations
+
