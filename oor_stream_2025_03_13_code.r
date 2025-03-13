99dc2867d4a32dc2c2ff86cd8f2e35978d26f038
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-03-13 17:14:58 +0100

    Commit: Thu 13 Mar 2025 05:14:58 PM CET

---
+++ oor_stream_2025_03_13_code.r
@@ -0,0 +1,17 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-03-13
+#
+# Topic(s):
+# - Statistical Rethinking (https://xcelab.net/rm/)
+# - Section(s): 5.2 - ?
+#
+# last updated: 2025-03-13
+
+############################################################################
+
+### 5.2: Masked relationship
+
