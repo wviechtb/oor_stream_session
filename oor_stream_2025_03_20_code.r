3ccc560b7377016314e2d288beb4ebc02c3f218f
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-03-20 17:12:53 +0100

    Commit: Thu 20 Mar 2025 05:12:53 PM CET

---
+++ oor_stream_2025_03_20_code.r
@@ -0,0 +1,31 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-03-20
+#
+# Topic(s):
+# - the tinyplot package
+# - the tidyplots package (if there is time)
+#
+# last updated: 2025-03-20
+
+############################################################################
+
+### tinyplot package
+
+# relevant links:
+# - https://cran.r-project.org/package=tinyplot
+# - https://grantmcdermott.com/tinyplot/
+
+# install the tinyplot and palmerpenguins packages
+install.packages("tinyplot")
+install.packages("palmerpenguins")
+
+# load the packages
+library(tinyplot)
+library(palmerpenguins)
+
+############################################################################
+
