1fe732d0eccfea234dbec58435eb5772edc7a92f
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-03-27 17:51:43 +0100

    Commit: Thu 27 Mar 2025 05:51:43 PM CET

---
+++ oor_stream_2025_03_27_code.r
@@ -0,0 +1,15 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-03-27
+#
+# Topic(s):
+# - new features in the metafor package (version 4.8-0)
+#
+# last updated: 2025-03-27
+
+############################################################################
+
+
