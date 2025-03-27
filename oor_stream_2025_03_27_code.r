f2c60e8b4defb3cdaf930b364280d8242c77e1e0
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-03-27 18:31:44 +0100

    Commit: Thu 27 Mar 2025 06:31:44 PM CET

--- oor_stream_2025_03_27_code.r
+++ oor_stream_2025_03_27_code.r
@@ -12,4 +12,33 @@
 
 ############################################################################
 
+# install the metafor package
+#install.packages("metafor")
 
+# load the metafor package
+library(metafor)
+
+# copy BCG vaccine meta-analysis data to 'dat'
+dat <- dat.bcg
+
+# tpos  - number of TB positive cases in the treated (vaccinated) group
+# tneg  - number of TB negative cases in the treated (vaccinated) group
+# cpos  - number of TB positive cases in the control (non-vaccinated) group
+# cneg  - number of TB negative cases in the control (non-vaccinated) group
+#
+# these variables denote the values in 2x2 tables of the form:
+#
+#           TB+    TB-
+#         +------+------+
+# treated | tpos | tneg |
+#         +------+------+
+# control | cpos | cneg |
+#         +------+------+
+
+# calculate log risk ratios and corresponding sampling variances (and use
+# the 'slab' argument to store study labels as part of the data frame)
+dat <- escalc(measure="RR", ai=tpos, bi=tneg, ci=cpos, di=cneg,
+              data=dat, slab=paste(author, year, sep=", "))
+
+### fit random-effects model
+res <- rma(yi, vi, data=dat)
