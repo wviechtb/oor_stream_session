8c3d1323825508613b4955193443e04f839cb3dc
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-04-10 21:17:38 +0200

    Commit: Thu 10 Apr 2025 09:17:38 PM CEST

--- oor_stream_2025_04_10_code.r
+++ oor_stream_2025_04_10_code.r
@@ -133,12 +133,12 @@ pairs(~ kcal.per.g + perc.fat + perc.lactose, data=dat, pch=19, col="#1e59ae")
 
 ### 6.2: Post-treatment bias
 
+# simulate data for the plant treatment example
 set.seed(71)
 N <- 100 # number of plants
 h0 <- rnorm(N, mean=10, sd=2) # simulate initial heights
 treatment <- rep(0:1, each=N/2) # assign treatments
-fungus <- rbinom(N, size=1, prob=0.5 - treatment*0.4)
-h1 <- h0 + rnorm(N, 5 - 3*fungus)
-# compose a clean data frame
-d <- data.frame( h0=h0 , h1=h1 , treatment=treatment , fungus=fungus )
-precis(d)
+fungus <- rbinom(N, size=1, prob=0.5-treatment*0.4) # simulate fungus variable
+h1 <- h0 + rnorm(N, mean=5-3*fungus) # simulate final height
+dat <- data.frame(h0=h0, h1=h1, treatment=treatment, fungus=fungus)
+precis(dat, prob=0.95)
