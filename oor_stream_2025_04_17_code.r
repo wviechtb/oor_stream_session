00ac6a9389d236ffcf044901ed03ec2b8d937e61
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-04-17 18:33:53 +0200

    Commit: Thu 17 Apr 2025 06:33:53 PM CEST

--- oor_stream_2025_04_17_code.r
+++ oor_stream_2025_04_17_code.r
@@ -24,15 +24,29 @@ library(rstanarm)
 logit <- qlogis
 invlogit <- plogis
 
+# Figure 13.1(a):
+xs <- seq(-6, 6, length.out=1000)
+ys <- invlogit(xs)
+par(mar=c(5,5,4,2), xaxs="i", yaxs="i")
+plot(xs, ys, type="l", bty="l", xlab="x", ylab=expression("logit"^-1 * (x)), ylim=c(0,1))
+title(expression(y == logit^-1 * (x)))
+segments(0, 0, 0, invlogit(0), lty="dotted")
+segments(-6, invlogit(0), 0, invlogit(0), lty="dotted")
+
 ## Example: modeling political preference given income
 
 # download the dataset if it doesn't already exist
 if (!file.exists("nes.txt")) download.file("https://raw.githubusercontent.com/avehtari/ROS-Examples/refs/heads/master/NES/data/nes.txt", destfile="nes.txt")
 
 # read in the dataset
-dat <- read.csv("student-merged.csv")
+dat <- read.table("nes.txt", header=TRUE)
 
 # inspect the first six rows of the dataset
 head(dat)
 
-https://github.com/avehtari/ROS-Examples/raw/refs/heads/master/NES/data/nes.txt
+# keep only the data from 1992 and exclude respondents who preferred other candidates or had no opinion
+ok <- dat$year==1992 & !is.na(nes$rvote) & !is.na(nes$dvote) & (nes$rvote==1 | nes$dvote==1)
+nes92 <- nes[ok,]
+
+
+plot(jitter(rvote, amount=0.05) ~ jitter(income, amount=.05), data=dat, bty="l", pch=19, cex=0.2)
