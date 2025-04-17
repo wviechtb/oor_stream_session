40a52e18ecb73562b80beac216090b9d55a9e40f
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-04-17 17:21:48 +0200

    Commit: Thu 17 Apr 2025 05:21:48 PM CEST

---
+++ oor_stream_2025_04_17_code.r
@@ -0,0 +1,22 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-04-17
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 13.1 - ?
+#
+# last updated: 2025-04-17
+
+############################################################################
+
+# load the rstanarm package
+library(rstanarm)
+
+############################################################################
+
+### 13.1: Logistic regression with a single predictor
+
