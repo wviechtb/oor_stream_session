8ad696eba1c5109be97402cbf137d9d6e487902b
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-08 18:36:04 +0200

    Commit: Thu 08 May 2025 06:36:04 PM CEST

--- oor_stream_2025_05_08_code.r
+++ oor_stream_2025_05_08_code.r
@@ -27,4 +27,12 @@ precis(dat)
 # inspect the code for sim_happiness() to see how exactly the data are simulated
 sim_happiness
 
-plot(happiness ~ age, data=dat, bg=ifelse(married==1, "#1e59ae", "white"))
+# Figure 6.4: plot of age versus happiness with blue dots corresponding to
+# married individuals
+plot(happiness ~ age, data=dat, pch=21, bg=ifelse(married==1, "#1e59ae", "white"))
+
+# make a copy of data that only incldues the adults (age >= 18)
+dat2 <- dat[dat$age >= 18,]
+
+# rescale the age variable so 0 corresponds to 18 and 1 corresponds to 65
+dat2$A <- (dat2$age - 18) / (65 - 18)
