351952aacd6a4750cf295aef53de5c9906013157
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-08 20:22:24 +0200

    Commit: Thu 08 May 2025 08:22:24 PM CEST

--- oor_stream_2025_05_08_code.r
+++ oor_stream_2025_05_08_code.r
@@ -126,15 +126,17 @@ b_GP <- 1 # direct effect of G on P
 b_GC <- 0 # direct effect of G on C
 b_PC <- 1 # direct effect of P on C
 b_UP <- 4 # direct effect of U on P
-b_UC <- 2 # direct effect of U on C
+b_UC <- 0 # direct effect of U on C
 
 # simulate the data
-set.seed(1)
+#set.seed(1)
 U <- rnorm(N)
 G <- rnorm(N, b_UG*U)
 P <- rnorm(N, b_GP*G + b_UP*U)
 C <- rnorm(N, b_PC*P + b_GC*G + b_UC*U)
 dat <- data.frame(C=C, P=P, G=G, U=U)
+
+summary(lm(C ~ P + G))
 
 # define and fit the model predicting C from P and G and inspect the results
 res <- quap(alist(C ~ dnorm(mu, sigma),
@@ -144,8 +146,7 @@ res <- quap(alist(C ~ dnorm(mu, sigma),
                   sigma ~ dexp(1)), data=dat)
 precis(res)
 
-# - if b_UG = b_UP (i.e., the unmeasured U affects G and P equally), then we
-#   find above that the estimate of b_GC is unbiased
+
 # - if b_UC = 0 (i.e., U affects G and P but not C), then b_UG and b_UP can be
 #   different and we again do not get any bias in b_GC
 
