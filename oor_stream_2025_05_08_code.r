865fb39e725530e69f628d37cf5594f4bbb997ec
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-08 17:19:56 +0200

    Commit: Thu 08 May 2025 05:19:56 PM CEST

---
+++ oor_stream_2025_05_08_code.r
@@ -0,0 +1,20 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-05-08
+#
+# Topic(s):
+# - Statistical Rethinking (https://xcelab.net/rm/)
+# - Section(s): 6.3 - ?
+#
+# last updated: 2025-05-08
+
+############################################################################
+
+# load the rethinking package
+library(rethinking)
+
+############################################################################
+
