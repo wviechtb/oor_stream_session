f1633f7fa8bf1714bd402b6f6d7e5d500b817a49
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-15 20:38:47 +0200

    Commit: Thu 15 May 2025 08:38:47 PM CEST

--- oor_stream_2025_05_15_code.r
+++ oor_stream_2025_05_15_code.r
@@ -250,4 +250,9 @@ sav
 sav$mle[sav$mle <= 0] <- 0
 sav$mle[sav$mle >= 1] <- 1
 
-
+# plot the MLE and Bayesian slope estimates; the dotted line is the true slope
+# and the blue line is the prior mean for the Bayesian model
+par(bty="l")
+stripchart(data.frame(apply(sav, 2, jitter, amount=0.02)), pch=19, vertical=TRUE, method="jitter")
+abline(h=0.8, lty="dotted")
+segments(1.8, 0.5, 2.2, 0.5, lwd=5, col="blue")
