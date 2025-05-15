f88d44a9f73b4aaa2a85837e89aa703ef93882e8
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-15 18:34:08 +0200

    Commit: Thu 15 May 2025 06:34:08 PM CEST

--- oor_stream_2025_05_15_code.r
+++ oor_stream_2025_05_15_code.r
@@ -20,3 +20,13 @@ library(rstanarm)
 
 ### 13.4: Latent-data formulation
 
+# see Wikipedia for some details about the logistic distribution:
+# https://en.wikipedia.org/wiki/Logistic_distribution
+#
+# in particular, note that the variance of a random variable following a
+# logistic distribution is equal to s^2 * pi^2 / 3, where s is the 'scale'
+# parameter
+
+# Figure 13.5: plot of the pdf from a logistic distribution
+xs <- seq(-6, 6, length.out=1000)
+ys <- dlogis(xs, location=0, scale=sqrt(3)/pi)
