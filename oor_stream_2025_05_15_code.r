89f557cae822450faa3067ea5e6b570533dcece0
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-15 21:24:59 +0200

    Commit: Thu 15 May 2025 09:24:59 PM CEST

--- oor_stream_2025_05_15_code.r
+++ oor_stream_2025_05_15_code.r
@@ -321,7 +321,7 @@ logLik(res)
 # this is the same as getting the predicted probabilities from this model
 # (which are all 0.5) and then computing the log score
 pred <- predict(res, type="response")
-sum(dat$rvote*log(pred) + (1-dat$rvote)*log(1 - pred))
+sum(dat$rvote*log(pred) + (1-dat$rvote)*log(1-pred))
 
 # log score for the model where we just use the information how many people
 # actually voted for Bush versus Clinton
@@ -332,6 +332,11 @@ res <- glm(rvote ~ 1, data=dat, family=binomial(link="logit"))
 logLik(res)
 
 # note that the log likelihood is the same as the log score for this model
+
+# this is the same as getting the predicted probabilities from this model
+# (which are all ~0.405) and then computing the log score
+pred <- predict(res, type="response")
+sum(dat$rvote*log(pred) + (1-dat$rvote)*log(1-pred))
 
 
 
