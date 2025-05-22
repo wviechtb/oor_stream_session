687d1276241eae4c94d6c490722f36ffe56adc6e
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-22 18:41:44 +0200

    Commit: Thu 22 May 2025 06:41:44 PM CEST

--- oor_stream_2025_05_22_code.r
+++ oor_stream_2025_05_22_code.r
@@ -22,3 +22,6 @@ library(rethinking)
 
 ## 7.1.1: More parameters (almost) always improve fit
 
+dat <- data.frame(species = c("afarensis", "africanus", "habilis", "boisei", "rudolfensis", "ergaster", "sapiens"),
+                  brain   = c(438, 452, 612, 521, 752, 871, 1350),
+                  mass    = c(37.0, 35.5, 34.5, 41.5, 55.5, 61.0, 53.5))
