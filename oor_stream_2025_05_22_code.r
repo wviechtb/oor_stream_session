086bac73feba07f5bb49cbb2f6354e27ebf10db9
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-29 22:10:01 +0200

    Commit: Thu 29 May 2025 10:10:01 PM CEST

--- oor_stream_2025_05_22_code.r
+++ oor_stream_2025_05_22_code.r
@@ -17,16 +17,7 @@ library(rethinking)
 ## 7.1.1: More parameters (almost) always improve fit
 
 # create the small dataset
-dat <- data.frame(species = c(
-  "afarensis", "africanus", "habilis", "boisei",
-  "rudolfensis", "ergaster", "sapiens"
-), pos = c(4, 3, 3, 4, 4, 3, 1), brain = c(
-  438,
-  452, 612, 521, 752, 871, 1350
-), mass = c(
-  37, 35.5, 34.5, 41.5, 55.5, 61,
-  53.5
-))
+dat <- data.frame(species = c("afarensis", "africanus", "habilis", "boisei", "rudolfensis", "ergaster", "sapiens"), pos = c(4, 3, 3, 4, 4, 3, 1), brain = c(438, 452, 612, 521, 752, 871, 1350), mass = c(37, 35.5, 34.5, 41.5, 55.5, 61, 53.5))
 
 # Figure 7.2: brain volume versus body size
 plot(brain ~ mass,
