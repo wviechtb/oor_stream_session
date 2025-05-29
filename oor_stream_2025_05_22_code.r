9496f8cedf1dcc0fc8aaaf74445633e0016025e3
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-29 22:16:04 +0200

    Commit: Thu 29 May 2025 10:16:04 PM CEST

--- oor_stream_2025_05_22_code.r
+++ oor_stream_2025_05_22_code.r
@@ -7,18 +7,14 @@ library(rethinking)
 
 ### 7.1: The problem with parameters
 
-## 7.1.1: More parameters (almost) always improve fit
+## 7.1.1: More parameters (almost) always improve
+## fit
 
 # create the small dataset
-dat <- data.frame(species = c(
-  "afarensis", "africanus", "habilis", "boisei",
-  "rudolfensis", "ergaster", "sapiens"
-), pos = c(4, 3, 3, 4, 4, 3, 1), brain = c(
-  438,
-  452, 612, 521, 752, 871, 1350
-), mass = c(
-  37, 35.5, 34.5, 41.5, 55.5, 61,
-  53.5
-))
+dat <- data.frame(species = c("afarensis", "africanus",
+   "habilis", "boisei", "rudolfensis", "ergaster", "sapiens"),
+   pos = c(4, 3, 3, 4, 4, 3, 1), brain = c(438, 452,
+      612, 521, 752, 871, 1350), mass = c(37, 35.5,
+      34.5, 41.5, 55.5, 61, 53.5))
 
 ############################################################################
