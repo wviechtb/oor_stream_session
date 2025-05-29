0daf8fa2f1ad1e3314c6c3f8e435d2d8b4950145
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-29 17:47:39 +0200

    Commit: Thu 29 May 2025 05:47:39 PM CEST

---
+++ oor_stream_2025_05_29_code.r
@@ -0,0 +1,19 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-05-29
+#
+# Topic(s):
+# - syntax style guides and syntax formatting
+# - packages styler and lintr
+# - the R formatter Air
+#
+# last updated: 2025-05-29
+
+############################################################################
+
+# - https://cran.r-project.org/package=styler
+# - https://cran.r-project.org/package=lintr
+# - https://posit-dev.github.io/air/
