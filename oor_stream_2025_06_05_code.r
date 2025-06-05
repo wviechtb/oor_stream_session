fc142a8e915a7b1ff3c3ff6e838734e51e2627a0
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-06-05 17:33:16 +0200

    Commit: Thu 05 Jun 2025 05:33:16 PM CEST

---
+++ oor_stream_2025_06_05_code.r
@@ -0,0 +1,22 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-06-05
+#
+# Topic(s):
+# - Regression and Other Stories (https://avehtari.github.io/ROS-Examples/)
+# - Section(s): 13.7 - ?
+#
+# last updated: 2025-06-05
+
+############################################################################
+
+# load the rstanarm package
+library(rstanarm)
+
+############################################################################
+
+### 13.7: Building a logistic regression model: wells in Bangladesh
+
