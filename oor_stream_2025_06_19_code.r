d3d9c0c8ffc9c49ab091af6fdfc4928a3a0c47f2
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-06-19 18:33:09 +0200

    Commit: Thu 19 Jun 2025 06:33:09 PM CEST

--- oor_stream_2025_06_19_code.r
+++ oor_stream_2025_06_19_code.r
@@ -42,7 +42,7 @@ simdata <- function(n) {
 # with a model without any predictor, then one predictor, two predictors, all
 # the way up to all 4 predictors
 
-fitmodels <- function(dat) {
+fitmodels <- function(dat, betasd=1) {
 
    res1 <- quap(alist(y ~ dnorm(mu, 1),
                       mu <- a,
@@ -50,20 +50,61 @@ fitmodels <- function(dat) {
    res2 <- quap(alist(y ~ dnorm(mu, 1),
                       mu <- a + b[1]*X1,
                       a ~ dnorm(0, 1),
-                      b ~ dnorm(0, 1)), data=dat, start=list(b=rep(0,1)))
+                      b ~ dnorm(0, betasd)), data=dat, start=list(b=rep(0,1)))
    res3 <- quap(alist(y ~ dnorm(mu, 1),
                       mu <- a + b[1]*X1 + b[2]*X2,
                       a ~ dnorm(0, 1),
-                      b ~ dnorm(0, 1)), data=dat, start=list(b=rep(0,2)))
+                      b ~ dnorm(0, betasd)), data=dat, start=list(b=rep(0,2)))
    res4 <- quap(alist(y ~ dnorm(mu, 1),
                       mu <- a + b[1]*X1 + b[2]*X2 + b[3]*X3,
                       a ~ dnorm(0, 1),
-                      b ~ dnorm(0, 1)), data=dat, start=list(b=rep(0,3)))
+                      b ~ dnorm(0, betasd)), data=dat, start=list(b=rep(0,3)))
    res5 <- quap(alist(y ~ dnorm(mu, 1),
                       mu <- a + b[1]*X1 + b[2]*X2 + b[3]*X3 + b[4]*X4,
                       a ~ dnorm(0, 1),
-                      b ~ dnorm(0, 1)), data=dat, start=list(b=rep(0,4)))
+                      b ~ dnorm(0, betasd)), data=dat, start=list(b=rep(0,4)))
    res <- list(res1, res2, res3, res4, res5)
    return(res)
 
 }
+
+# now we repeat the simulation described (note: this takes quite a bit of
+# time, so the code prints the iteration number to keep track of progress;
+# running this with 100 iterations is already sufficient to see the same
+# pattern as shown in the book)
+
+iters <- 100
+
+dev.train <- matrix(NA_real_, nrow=iters, ncol=5)
+dev.test  <- matrix(NA_real_, nrow=iters, ncol=5)
+
+n <- 20
+
+for (j in 1:iters) {
+
+   print(j)
+   dat <- simdata(n)
+   res <- fitmodels(dat)
+   lppd <- sapply(res, function(m) sum(lppd(m)))
+   dev.train[j,] <- -2 * lppd
+   dat <- simdata(n)
+   lppd <- sapply(res, function(m) sum(lppd(m, data=dat)))
+   dev.test[j,] <- -2 * lppd
+
+}
+
+dev.train.mean <- apply(dev.train, 2, mean)
+dev.train.lo   <- dev.train.mean - apply(dev.train, 2, sd)
+dev.train.hi   <- dev.train.mean + apply(dev.train, 2, sd)
+
+dev.test.mean  <- apply(dev.test, 2, mean)
+dev.test.lo    <- dev.test.mean - apply(dev.test, 2, sd)
+dev.test.hi    <- dev.test.mean + apply(dev.test, 2, sd)
+
+# Figure 7.6 (left): deviance in and out of sample for the 5 models for n=20
+plot(NA, xlim=c(0.8,5.2), ylim=c(min(dev.train.lo, dev.test.lo), max(dev.train.hi, dev.test.hi)),
+     xlab="number of parameters", ylab="deviance", main=paste("N =", n))
+segments(1:5, dev.train.lo, 1:5, dev.train.hi, col="#1e59ae", lwd=2)
+points(1:5, dev.train.mean, pch=19, col="#1e59ae")
+segments(1:5 + 0.1, dev.test.lo, 1:5 + 0.1, dev.test.hi, lwd=2)
+points(1:5 + 0.1, dev.test.mean, pch=21)
