556d89da1b282453d2945db11d6519e1500ac0a0
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-06-19 17:33:04 +0200

    Commit: Thu 19 Jun 2025 05:33:04 PM CEST

---
+++ oor_stream_2025_06_19_code.r
@@ -0,0 +1,22 @@
+############################################################################
+
+# Open Online R Stream (https://www.wvbauer.com/doku.php/live_streams)
+#
+# By:   Wolfgang Viechtbauer (https://www.wvbauer.com)
+# Date: 2025-06-19
+#
+# Topic(s):
+# - Statistical Rethinking (https://xcelab.net/rm/)
+# - Section(s): 7.3 - ?
+#
+# last updated: 2025-06-19
+
+############################################################################
+
+# load the rethinking package
+library(rethinking)
+
+############################################################################
+
+### 7.3: Golem taming: regularization
+
