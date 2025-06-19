b47d1d67f19ac1909685db5f52eca660d5917a21
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-06-19 18:19:05 +0200

    Commit: Thu 19 Jun 2025 06:19:05 PM CEST

--- oor_stream_2025_06_19_code.r
+++ oor_stream_2025_06_19_code.r
@@ -20,3 +20,10 @@ library(rethinking)
 
 ### 7.3: Golem taming: regularization
 
+# Figure 7.7: plots of the density of a normal distribution with mean 0 and
+# standard deviations of 1, 0.5, and 0.2
+plot(NA, xlim=c(-3,3), ylim=c(0,2), xlab="parameter value", ylab="density",
+     bty="l", las=1)
+curve(dnorm(x, mean=0, sd=1),   from=-3, to=3, n=1001, add=TRUE, lty="dashed")
+curve(dnorm(x, mean=0, sd=0.5), from=-3, to=3, n=1001, add=TRUE)
+curve(dnorm(x, mean=0, sd=0.2), from=-3, to=3, n=1001, add=TRUE, lwd=3)
